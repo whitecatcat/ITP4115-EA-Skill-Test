f86dc33f477613aff3ee318318f02a6a1b7f2815
EC2 Default User <ec2-user@ip-172-31-65-201.ec2.internal>
2021-05-12 10:28:40 +0000

    finish commit

--- app/models.py
+++ app/models.py
@@ -5,6 +5,7 @@ from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 
 
+
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
--- setup.sh
+++ setup.sh
@@ -1,1 +1,4 @@
-#To be finished
+ecurity_group=$(ec2-metadata -s | cut -d " " -f 2);
+aws ec2 authorize-security-group-ingress --group-name $security_group --protocol tcp --port 5000 --cidr 0.0.0.0/0
+aws ec2 authorize-security-group-ingress --group-name $security_group --protocol tcp --port 8000 --cidr 0.0.0.0/0
+
